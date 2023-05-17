0a7d14a1f3c994abb2ff50ae9a8dab4f63145e8e
AbdullahFathan <abdullahfathan013@gmail.com>
2023-05-17 21:52:13 +0700

    done: show result summarize article

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -10,6 +10,7 @@ const Demo = () => {
   });
 
   const [allArticle, setAllArticle] = useState([]);
+  const [copied, setCopied] = useState("");
 
   const [getSummary, { eror, isFetching }] = useLazyGetSummaryQuery();
 
@@ -36,6 +37,12 @@ const Demo = () => {
 
       localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
+  };
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
   };
 
   return (
@@ -77,9 +84,9 @@ const Demo = () => {
                 onClick={() => setArticle(item)}
                 className="link_card"
               >
-                <div className="copy_btn">
+                <div className="copy_btn" onClick={() => handleCopy(item.url)}>
                   <img
-                    src={copy}
+                    src={copied === item.url ? tick : copy}
                     alt="copy_icon"
                     className="w-[40$] h-[40%] object-contain"
                   />
@@ -93,7 +100,36 @@ const Demo = () => {
         </div>
       </div>
       {/*Display Result */}
-      <div className="my-10 max-w-full flex justify-center items-center "></div>
+      <div className="my-10 max-w-full flex justify-center items-center ">
+        {isFetching ? (
+          <img
+            src={loader}
+            alt="loader_icon"
+            className="w-20 h-20 object-contain"
+          />
+        ) : eror ? (
+          <p className="font-inter font-bold text-center text-black">
+            There was a eror.... <br />{" "}
+            <span className="font-satoshi font-normal text-gray-700">
+              {eror?.data?.eror}
+            </span>{" "}
+          </p>
+        ) : (
+          article.summary && (
+            <div className="flex flex-col gap-3">
+              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+                Article <span className="blue_gradient">Summary</span>
+              </h2>
+              <div className="summary_box">
+                <p className="font-inter font-medium text-sm text-gray-700">
+                  {" "}
+                  {article.summary}
+                </p>
+              </div>
+            </div>
+          )
+        )}
+      </div>
     </section>
   );
 };
